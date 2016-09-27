27a96f4d388d928b09282e9bae03e47106302181
RyS0315 <Riley.Stadel1@gmail.com>
2016-09-27 18:26:46 -0400

    Added lab2

---
+++ Labs/Lab 2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Michael Phelps</title>
+
+</head>
+<body>
+<?php
+$logo = "images/goat.png";
+$welcome  ="Welcome to Michael Phelps's Webpage";
+$message = "Phelps is G.O.A.T. (Greatest Olympian of All Time)";
+echo"<h1>" . $welcome. "</h1>";
+echo "<img border=\"0\" src= ". $logo . " alt= \"Phelps\">";
+echo "<h4>". $message."</h4>";
+?>
+
+</body>
+
+</html>
